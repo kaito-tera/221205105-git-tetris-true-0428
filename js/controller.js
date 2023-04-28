970fdcdf8725e98ffd740452b05bb4e3bd635a8f
kaito Teramachi <221205105@ccmailg.meijo-u.ac.jp>
2023-04-28 14:15:30 +0900

    キー変更

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,10 @@
 document.body.onkeydown = function( e ) {
     var keys = {
-        37: 'left',
-        39: 'right',
-        40: 'down',
-        38: 'rotate',
-        32: 'drop'
+        65: 'left',
+        68: 'right',
+        83: 'down',
+        76: 'rotate',
+        38: 'drop'
     };
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
